7bba739192574c65e449272ebb5a4a5df934f737
Ethan <ethan_gustafson@outlook.com>
2020-04-16 22:57:05 -0400

    removed initial react templates, added google font, created 1 route for App Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Route exact path="/" component={App}/>
+  </Router>,
   document.getElementById('root')
 );
 
