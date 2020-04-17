79b7f3f28e08dfd8ff2b3d182a24fa6e41d105f8
Ethan <ethan_gustafson@outlook.com>
2020-04-17 01:50:35 -0400

    added user data to show on profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 export default class Profile extends React.Component{
     
     render(){
-        const currentUser = JSON.parse(localStorage.getItem("loggedIn"))
-        console.log(currentUser.username)
+        const currentUser = JSON.parse(localStorage.getItem("loggedIn")).userData
         return(
             <div>
                 <h1><Link to="/home">vacationMe</Link></h1>
                 {/* <h1><Link to="/destinations">Destinations</Link></h1> */}
-                <h1>{currentUser.userData.username}</h1>
+                <h1>{currentUser.username}</h1>
+                <p>{currentUser.name}</p>
+                <p>{currentUser.email}</p>
             </div>
         )
     }
