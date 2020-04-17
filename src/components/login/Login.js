6f862abe35b057b484dc2f4c498e6b7e3bb048e5
Ethan <ethan_gustafson@outlook.com>
2020-04-16 23:14:16 -0400

    created simple login form which updates component state upon input onChange

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 export default class Login extends React.Component{
+
+    state = {
+        email: "",
+        password: ""
+    }
+
+    handleOnChange = event => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render(){
         return(
             <div id="loginform">
                 <form>
-                    <input type="text"/>
+                    <input onChange={this.handleOnChange}
+                     type="email" 
+                     name="email" 
+                     value={this.state.email} 
+                     placeholder="Email"
+                     />
+                    <input onChange={this.handleOnChange}
+                     type="password" 
+                     name="password" 
+                     value={this.state.password}
+                     placeholder="Password"
+                     />
                     <input type="submit"/>
                 </form>
             </div>
