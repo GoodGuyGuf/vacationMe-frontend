bb3ff02e5782532231ae5b82fc3443a900f336b5
Ethan <ethan_gustafson@outlook.com>
2020-04-18 15:31:11 -0400

    added simple fetch for PostForm, added labels for inputs

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -18,14 +18,45 @@ export default class PostForm extends React.Component{
         })
     }
 
+    handleOnSubmit = event => {
+        event.preventDefault();
+        const post = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(this.state)
+        }
+
+        fetch("http://localhost:3000/posts", post)
+        .then(response => response.json())
+        .then(json => {
+            if (json.message === "Unable to save."){
+                alert("Unable to save.")
+            } else {
+                console.log(json.data)
+                // const postData = {
+                //     id: json.data.id,
+                //     name: json.data.attributes.name, 
+                //     username: json.data.attributes.username, 
+                //     email: json.data.attributes.email
+                // }
+            }
+        })
+    }
+
     render(){
         return (
             <div>
-                <form>
-                <h3>Write a new post:</h3>
+                <form onSubmit={this.handleOnSubmit}>
+                <h3>Write a new post: </h3>
+                <label for="title" >Title </label>
                     <input onChange={this.handleOnChange} type="text" name="title" value={this.state.title} /><br/>
-                    <input onChange={this.handleOnChange} type="text" name="location" value={this.state.title}/><br/>
-                    <input onChange={this.handleOnChange} type="text" name="caption" value={this.state.title}/><br/>
+                    <label for="location" >Location </label>
+                    <input onChange={this.handleOnChange} type="text" name="location" value={this.state.location}/><br/>
+                    <label for="caption" >Caption </label>
+                    <textarea onChange={this.handleOnChange} type="text" name="caption" value={this.state.caption}/><br/>
                     <input type="submit" value="Post"/>
                 </form>
             </div>
