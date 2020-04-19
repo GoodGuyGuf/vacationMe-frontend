203f169db5aae967788672b7ae8c6937e7c6007e
Ethan <ethan_gustafson@outlook.com>
2020-04-18 22:45:39 -0400

    changed sizes of profile/logout link on Home page

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
         <div id="NavBar">
         <h1 id="logo"><Link to="/home">vacationMe</Link></h1>
         <h1 id="profile"><Link to="/profile">Profile</Link></h1>
-        <h3 id="logout"><Link to="/" onClick={logout}>Logout</Link></h3>
+        <h1 id="logout"><Link to="/" onClick={logout}>Logout</Link></h1>
         {/* <h1><Link to="/destinations">Destinations</Link></h1> */}
         </div>
     )
