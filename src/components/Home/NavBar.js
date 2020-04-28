241c6c4b517ab00d4c68214f5cb53809945dad31
Ethan <ethan_gustafson@outlook.com>
2020-04-27 23:55:49 -0400

    added destinations link for destination filtering

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -12,6 +12,7 @@ export default class NavBar extends React.Component {
             <div id="NavBar">
                 <h1 id="homeLink"><Link to="/" >Home</Link></h1>
                 <h1 id="logo">vacationMe</h1>
+                <h1 id="destinationFilter"><Link to="/destinations">Destinations</Link></h1>
                 <h1 id="profile"><Link to="/profile">Profile</Link></h1>
                 <h1 id="logout"><Link to="/login" onClick={this.logout}>Logout</Link></h1>
             </div>
