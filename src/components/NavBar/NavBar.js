b44b5bf23fe73b6beef1e3f13324da0a224a88c2
Ethan <ethan_gustafson@outlook.com>
2020-06-21 02:14:59 -0400

    added semantic html + readability to NavBar.js

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,13 +5,15 @@ export default class NavBar extends React.Component {
 
     render(){
         return (
-            <div id="NavBar">
-                <h1 id="homeLink"><Link to="/" >Home</Link></h1>
-                <h1 id="logo">vacationMe</h1>
-                <h1 id="destinationFilter"><Link to="/destinations">Destinations</Link></h1>
-                <h1 id="profile"><Link to="/profile">Profile</Link></h1>
-                <h1 id="logout"><Link to="/login" onClick={this.props.logoutUser}>Logout</Link></h1>
-            </div>
+            <header>
+                <nav id="NavBar" role="navigation" aria-label="site feature links">
+                    <h1 id="homeLink"><Link to="/" >Home</Link></h1>
+                    <h1 id="logo">vacationMe</h1>
+                    <h1 id="destinationFilter"><Link to="/destinations">Destinations</Link></h1>
+                    <h1 id="profile"><Link to="/profile">Profile</Link></h1>
+                    <h1 id="logout"><Link to="/login" onClick={this.props.logoutUser}>Logout</Link></h1>
+                </nav>
+            </header>
         )
     }
 }
