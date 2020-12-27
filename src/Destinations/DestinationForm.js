56cb58b97c700e3664ec6e478050e6a3b7b7f58b
Ethan <ethan_gustafson@outlook.com>
2020-12-27 15:41:26 -0500

    format indentation for DestinationForm

--- src/Destinations/DestinationForm.js
+++ src/Destinations/DestinationForm.js
@@ -2,48 +2,50 @@ import React, { Component } from 'react';
 import PostsList from '../components/Posts/PostsList';
 
 export default class DestinationForm extends Component {
+  state = {
+    search: "",
+    isSubmitted: false
+  }
 
-    state = {
-        search: "",
-        isSubmitted: false
-    }
+  handleOnChange = event => {
+    this.setState({
+      search: event.target.value,
+      isSubmitted: false
+    })
+  }
 
-    handleOnChange = event => {
-        this.setState({
-            search: event.target.value,
-            isSubmitted: false
-        })
-    }
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.setState(state => ({
+      search: state.search,
+      isSubmitted: true
+    }))
+  }
 
-    handleOnSubmit = event => {
-        event.preventDefault()
-        this.setState(state => ({
-            search: state.search,
-            isSubmitted: true
-        }))
-    }
+  filterPosts = () => {
+    const filteredPosts = this.props.posts.filter(post => post.location.includes(this.state.search))
+    return (
+      <PostsList posts={filteredPosts} users={this.props.users}
+        currentUser={this.props.currentUser}
+        comments={this.props.comments}
+        createComment={this.props.createComment}
+      />
+    )
+  }
 
-    filterPosts = () => {
-        const filteredPosts = this.props.posts.filter(post => post.location.includes(this.state.search))
-        return (<PostsList posts={filteredPosts} users={this.props.users}
-            currentUser={this.props.currentUser}
-            comments={this.props.comments}
-            createComment={this.props.createComment}/>)
-    }
-
-    render(){
-        return(
-            <>
-                <section>
-                    <form id="destinationFilterForm" onSubmit={this.handleOnSubmit}>
-                        <h3>Search For Destinations:</h3>
-                        <input type="text" name="search" placeholder="Search" onChange={this.handleOnChange} value={this.state.search}/>
-                        <input type="submit" value="Submit"/>
-                    </form>
-                </section>
-                
-                {this.state.isSubmitted && this.filterPosts()}
-            </>
-        )
-    }
+  render(){
+    return(
+      <>
+        <section>
+          <form id="destinationFilterForm" onSubmit={this.handleOnSubmit}>
+            <h3>Search For Destinations:</h3>
+            <input type="text" name="search" placeholder="Search" onChange={this.handleOnChange} value={this.state.search}/>
+            <input type="submit" value="Submit"/>
+          </form>
+        </section>
+        
+        {this.state.isSubmitted && this.filterPosts()}
+      </>
+    )
+  }
 }
